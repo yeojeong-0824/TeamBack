32b175ee0657954b2280c9db12240b0cbd0d3b9d
songseongbin <ssbin0916@naver.com>
2024-07-17 08:55:00 +0900

    Feat: 게시판 예외 추가

---
+++ src/main/java/com/example/demo/config/exception/board/NotFoundBoardException.java
@@ -0,0 +1,8 @@
+package com.example.demo.config.exception.board;
+
+public class NotFoundBoardException extends RuntimeException {
+
+    public NotFoundBoardException(String message) {
+        super(message);
+    }
+}
