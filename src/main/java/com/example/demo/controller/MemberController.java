5d35309a79de65b24c8a78bc01bf3d1013a82e02
mini <155148431+mini0192@users.noreply.github.com>
2024-07-19 04:22:23 +0900

    Feat: 이메일 보내는 로직 추가

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -2,15 +2,20 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.member.MemberRequest.*;
 import com.example.demo.dto.member.MemberResponse;
+import com.example.demo.security.email.EmailService;
 import com.example.demo.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -30,6 +35,7 @@ import java.util.List;
 public class MemberController {
 
     private final MemberService memberService;
+    private final EmailService emailService;
 
     @PostMapping
     @Operation(summary = "유저 생성") // 해당 메소드의 역할을 설명
@@ -71,7 +77,7 @@ public class MemberController {
                     @ApiResponse(responseCode = "409", description = "중복됨")
             }
     )
-    public ResponseEntity<String> checkDuplicated(@Parameter(content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)) // 메소드가 받는 파라미터는 Json 형식을 사용한다
+    public ResponseEntity<String> checkDuplicated(@Parameter(content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
                                                   @Valid @RequestBody DataConfirmMember taken,
                                                   HttpServletRequest request) {
 
@@ -86,6 +92,33 @@ public class MemberController {
 
         if(taken.email() != null && !memberService.checkDuplicatedEmail(taken.email()))
             return ResponseEntity.status(HttpStatus.CONFLICT).body("이메일이 중복됨");
+
+        return ResponseEntity.ok("중복되지 않음");
+    }
+
+    @PostMapping("/emailAuthed")
+    @Operation(summary = "이메일 인증")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "중복되지 않음"),
+                    @ApiResponse(responseCode = "400", description = "입력 값이 잘못됨"),
+                    @ApiResponse(responseCode = "409", description = "중복됨")
+            }
+    )
+    public ResponseEntity<String> emailAuthed(@Parameter(content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
+                                              @NotBlank @Size(min = 1, max = 50)
+                                              @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "유효한 이메일이 아닙니다.")
+                                              @Schema(example = "example@naver.com")
+                                              @RequestBody String email,
+                                              HttpServletRequest request) {
+
+        // ToDo: 이메일 인증 코드 생성 및 인증 절차
+
+        String ip = request.getRemoteAddr();
+        String title = "이메일 인증 코드";
+        String text = "이메일 인증 코드";
+
+        emailService.sendEmail(email, title, text);
 
         return ResponseEntity.ok("중복되지 않음");
     }
