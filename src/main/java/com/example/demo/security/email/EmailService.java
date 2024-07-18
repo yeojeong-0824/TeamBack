9c564e64b4d5750e38cf4adbc5fdf389e5c93e6e
mini <155148431+mini0192@users.noreply.github.com>
2024-07-19 07:32:32 +0900

    Ci: Ci 에러 확인

--- src/main/java/com/example/demo/security/email/EmailService.java
+++ src/main/java/com/example/demo/security/email/EmailService.java
@@ -3,7 +3,7 @@ package com.example.demo.security.email;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -11,24 +11,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailService {
 
-    private final JavaMailSender javaMailSender;
-
-    public void sendEmail(String toEmail, String title, String text) {
-        SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
-        try {
-            javaMailSender.send(emailForm);
-            log.info("이메일 전송 성공 to.{}, title. {}, text. {}", toEmail, title, text);
-        } catch (RuntimeException e) {
-            log.error("이메일 전송 실패 to.{}, title. {}, text. {}", toEmail, title, text);
-        }
-    }
-
-    private SimpleMailMessage createEmailForm(String toEmail, String title, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject(title);
-        message.setText(text);
-
-        return message;
-    }
+////    private final JavaMailSender javaMailSender;
+//
+//    public void sendEmail(String toEmail, String title, String text) {
+//        SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
+//        try {
+////            javaMailSender.send(emailForm);
+//            log.info("이메일 전송 성공 to.{}, title. {}, text. {}", toEmail, title, text);
+//        } catch (RuntimeException e) {
+//            log.error("이메일 전송 실패 to.{}, title. {}, text. {}", toEmail, title, text);
+//        }
+//    }
+//
+//    private SimpleMailMessage createEmailForm(String toEmail, String title, String text) {
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(toEmail);
+//        message.setSubject(title);
+//        message.setText(text);
+//
+//        return message;
+//    }
 }
