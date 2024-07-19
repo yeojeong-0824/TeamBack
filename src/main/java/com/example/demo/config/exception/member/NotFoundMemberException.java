37611c39051bcfc02d126415e5aebe1f7cd6881c
songseongbin <ssbin0916@naver.com>
2024-07-19 09:37:10 +0900

    Refactor: 보드 생성, 수정 dto 변경

---
+++ src/main/java/com/example/demo/config/exception/member/NotFoundMemberException.java
@@ -0,0 +1,8 @@
+package com.example.demo.config.exception.member;
+
+public class NotFoundMemberException extends RuntimeException {
+
+    public NotFoundMemberException(String message) {
+        super(message);
+    }
+}
