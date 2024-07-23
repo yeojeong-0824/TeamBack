9fb25fc2ba77eeb649b4308940acb41325137abf
songseongbin <ssbin0916@naver.com>
2024-07-23 18:48:51 +0900

    Refactor: 예외 처리

--- src/main/java/com/example/demo/service/CommentService.java
+++ src/main/java/com/example/demo/service/CommentService.java
@@ -1,8 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.config.exception.board.NotFoundBoardException;
-import com.example.demo.dto.comment.CommentRequest.*;
-import com.example.demo.dto.comment.CommentResponse.*;
+import com.example.demo.config.exception.comment.NotFoundCommentException;
+import com.example.demo.config.exception.member.NotFoundMemberException;
+import com.example.demo.dto.comment.CommentRequest.CommentSaveRequest;
+import com.example.demo.dto.comment.CommentResponse.CommentSaveResponse;
 import com.example.demo.entity.Board;
 import com.example.demo.entity.Comment;
 import com.example.demo.entity.Member;
@@ -23,9 +25,14 @@ public class CommentService {
 
     // 댓글 작성
     @Transactional
-    public CommentSaveResponse writeComment(CommentSaveRequest request, Long boardId, String memberName){
-        Member member = memberRepository.findByUsername(memberName).get();
-        Board board = boardRepository.findById(boardId).get();
+    public CommentSaveResponse writeComment(CommentSaveRequest request, Long boardId, String memberName) {
+
+        Member member = memberRepository.findByUsername(memberName)
+                .orElseThrow(() -> new NotFoundMemberException("해당 회원을 찾을 수 없습니다."));
+
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new NotFoundBoardException("해당 게시글을 찾을 수 없습니다."));
+
         Comment comment = toEntity(request, member, board);
         commentRepository.save(comment);
 
@@ -38,7 +45,10 @@ public class CommentService {
     // 댓글 수정
     @Transactional
     public CommentSaveResponse updateComment(CommentSaveRequest request, Long commentId) {
-        Comment comment = commentRepository.findById(commentId).get();
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new NotFoundCommentException("해당 댓글을 찾을 수 없습니다."));
+
         comment.update(request);
         return new CommentSaveResponse(comment);
     }
@@ -46,8 +56,12 @@ public class CommentService {
     // 댓글 삭제
     @Transactional
     public Long deleteComment(Long commentId) {
-        Comment comment = commentRepository.findById(commentId).get();
-        Board board = boardRepository.findById(comment.getBoard().getId()).get();
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new NotFoundCommentException("해당 댓글을 찾을 수 없습니다."));
+        Board board = boardRepository.findById(comment.getBoard().getId())
+
+                .orElseThrow(() -> new NotFoundBoardException("해당 게시글을 찾을 수 없습니다."));
 
         // 게시글 댓글 수 담소
         board.commentCountDown();
