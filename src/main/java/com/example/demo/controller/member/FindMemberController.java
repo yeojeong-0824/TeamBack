9efe669b7cb931c7695eee032af2cce55a5c8c78
mini <155148431+mini0192@users.noreply.github.com>
2024-07-28 16:28:13 +0900

    Fix: 비밀번호 재발급시 보안 강화

--- src/main/java/com/example/demo/controller/member/FindMemberController.java
+++ src/main/java/com/example/demo/controller/member/FindMemberController.java
@@ -9,9 +9,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -39,11 +41,19 @@ public class FindMemberController {
     public ResponseEntity<String> newPassword(@Size(min = 5, max = 30) @Schema(example = "user12")
                                               @PathVariable("username")
                                               String username,
+
+                                              @Size(min = 1, max = 50) @Schema(example = "example@naver.com")
+                                              @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+                                                      message = "유효한 이메일이 아닙니다.")
+                                              @RequestBody String email,
+
                                               HttpServletRequest request) {
         String ip = request.getRemoteAddr();
         log.info("{}: 새로운 비밀번호 발급 호출", ip);
 
         String savedMemberEmail = memberService.findMemberEmailByUsername(username);
+        if(!email.equals(savedMemberEmail)) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이메일이 잘못되었습니다.");
+
         String password = findMemberEmailService.createNewPassword();
 
         findMemberEmailService.sendNewPasswordEmail(savedMemberEmail, password);
