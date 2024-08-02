1b0be8b2e4f52c34f7e00ecf2cb5da9d3e9213d1
songseongbin <ssbin0916@naver.com>
2024-08-02 11:04:41 +0900

    Feat: 자동 완성 컨트롤러

---
+++ src/main/java/com/example/demo/controller/AutocompleteController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.autocomplete.AutocompleteResponse;
+import com.example.demo.service.AutocompleteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/autocomplete")
+public class AutocompleteController {
+
+    private final AutocompleteService autocompleteService;
+
+    public AutocompleteController(AutocompleteService autocompleteService) {
+        this.autocompleteService = autocompleteService;
+    }
+
+    @GetMapping("/{searchWord}")
+    @ResponseBody
+    public ResponseEntity<AutocompleteResponse> getAutocompleteList(@PathVariable String searchWord) {
+        return ResponseEntity.ok(autocompleteService.getAutocomplete(searchWord));
+    }
+}
