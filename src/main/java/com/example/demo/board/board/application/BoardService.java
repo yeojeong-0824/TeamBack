be61a529a2a685906785563da5a60f0ed99a370d
Go-EunLee <koa789456@gmail.com>
2024-08-08 13:31:43 +0900

    Chore : BordService 인터페이스 생성으로 Board 관련 파일 수정 BoardService 의 생성으로 메소드명 통합. BoardServiceImpl 의 CRUD 기능 void 로 리턴 그에 따라 Controller 코드 수정

--- src/main/java/com/example/demo/board/board/application/BoardService.java
+++ src/main/java/com/example/demo/board/board/application/BoardService.java
@@ -1,4 +1,14 @@
 package com.example.demo.board.board.application;
 
+import com.example.demo.board.board.presentation.dto.BoardRequest;
+import com.example.demo.board.board.presentation.dto.BoardResponse;
+import org.springframework.data.domain.Page;
+
 public interface BoardService {
+    void save(BoardRequest.DefaultBoard request, Long memberId);
+    void updateById(Long id, BoardRequest.BoardUpdateRequest request);
+    BoardResponse.BoardReadResponse findById(Long id);
+    Page<BoardResponse.BoardListResponse> findAll(int page);
+    Page<BoardResponse.BoardListResponse> findAllBySearchKeyword(String searchKeyword, String keyword, String sortKeyword, int page);
+    void deleteById(Long id);
 }
