f7862c2c3875bc9fe45757017c92631dce667302
mini <155148431+mini0192@users.noreply.github.com>
2024-08-24 16:49:21 +0900

    Ci: test

--- src/main/java/com/example/demo/member/email/application/EmailService.java
+++ src/main/java/com/example/demo/member/email/application/EmailService.java
@@ -16,7 +16,6 @@ public class EmailService {
 
     private final JavaMailSender javaMailSender;
 
-    //test
     private SimpleMailMessage createEmailForm(String toEmail, String title, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(toEmail);
