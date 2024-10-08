a222001aa212eaecdcfeab6bf8e05c07fb35858f
mini <155148431+mini0192@users.noreply.github.com>
2024-10-08 18:50:49 +0900

    Docs: Swagger 문서 수정

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public GroupedOpenApi group1() {
-        String[] paths = {"/api/vi/members", "/api/vi/members/**"};
+        String[] paths = {"/api/vi/members", "/api/v1/members/**"};
 
         return GroupedOpenApi.builder()
                 .group("유저 API")
@@ -24,7 +24,7 @@ public class SwaggerConfig {
 
     @Bean
     public GroupedOpenApi group2() {
-        String[] paths = {"/api/vi/boards", "/api/vi/boards/**"};
+        String[] paths = {"/api/vi/boards", "/api/v1/boards/**"};
 
         return GroupedOpenApi.builder()
                 .group("게시글 API")
