9274322f300f1d8a7a1119b1289fa35285c230f2
Go-EunLee <koa789456@gmail.com>
2024-10-30 20:21:57 +0900

    YJ1-55 Feat : token 예외 처리 및 오류 해결 -token 전체적인 코드 예외처리 -> 쉽게 처리하기 위해 throw 로 예외처리 -refresh token 을 새롭게 발급받는 코드에서 RefreshController 가 "/token" 경로가 생기면서 2개의 Refresh 쿠키가 생기는 것을 확인 -> cookie.setPath("/") 로 오류 해결 -@Cookie 어노테이션을 사용하여 쿠리를 가져왔지만 쿠키가 없을 시 500 에러를 보내는 것을 확인 -> 에러 통일화를 위해 Cookie 처리를 직접 해결

--- src/main/java/com/yeojeong/application/security/config/refreshtoken/application/RefreshTokenService.java
+++ src/main/java/com/yeojeong/application/security/config/refreshtoken/application/RefreshTokenService.java
@@ -2,11 +2,14 @@ package com.yeojeong.application.security.config.refreshtoken.application;
 
 import com.yeojeong.application.domain.member.presentation.dto.MemberDetails;
 import com.yeojeong.application.security.config.refreshtoken.domain.RefreshToken;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface RefreshTokenService {
     void save(RefreshToken data);
     RefreshToken findById(String token);
     void delete(RefreshToken token);
-    RefreshToken validRefresh(String token);
+    RefreshToken validRefresh(Cookie[] cookies, String token);
     String createRefresh(Long memberId, MemberDetails member);
 }
